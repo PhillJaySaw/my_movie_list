1039b803b66de8e4569217ec655b7c5e43977da0
PhillJaySaw <philljaysaw@gmail.com>
2016-10-29 01:05:47 +0200

    added main program file

---
+++ main.rb
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+=begin
+   This is my first ruby program.
+   It is a simple program that will collect
+   data about movies I have watched.
+   Every movie has a title, genre and rating
+   from 1 to 10.
+=end
+$my_movies = [] # this array holds all the movie objects
+
+class Movie
+   #
+   # This is the class for creating
+   # movie objects. Every object holds
+   # one movie title, the genre and rating.
+   # Every movie object is automaticly added
+   # to the my_movie array.
+   #
+   @@no_of_movies = 0
+   def initialize(title, genre, rating)
+      @title = title
+      @genre = genre
+      @rating = rating
+      @@no_of_movies += 1
+      $my_movies.push(self)
+   end
+
+   attr_accessor :title, :genre, :rating
+end
+
+def show_movie_list
+   # this method will show all the movies added
+   $my_movies.each do |element|
+      puts element.title
+   end
+end
+
+legend = Movie.new("Legend", "drama", 7)
+
+puts "The title of the movie is #{legend.title}"
+puts "It's a #{legend.genre}"
+puts "I rated the movie #{legend.rating} out of 10"
+
+
+the_shining = Movie.new("The Shining", "thriller", 8)
+interstellar = Movie.new("Interstellar", "drama", 9)
+lord_of_the_rings_1 = Movie.new("The Lord of the Rings - Fellowship of the ring", "fantasy", 10)
+
+show_movie_list
